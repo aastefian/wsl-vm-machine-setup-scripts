281187d2e2dc211d0e0388b4524451262f454617
Stefan Vilau <aastefian@gmail.com>
2024-10-04 09:55:28 +0300

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,8 +1,21 @@
-# Get docker here https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "\n\n ### Getting docker-compose ### \n\n"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-echo "\n\n ### Making them executable ### \n\n"
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+
+# Old stuff
+# echo "\n\n ### Getting docker-compose ### \n\n"
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# echo "\n\n ### Making them executable ### \n\n"
+# sudo chmod +x /usr/local/bin/docker-compose
+# docker-compose --version
