609f158174c8346891f4b72fec4517b209175bcc
Stefan Vilau <aastefian@gmail.com>
2024-10-11 11:40:14 +0300

    Create docker-cleanup.sh

---
+++ docker-cleanup.sh
@@ -0,0 +1,15 @@
+echo "Cleaning apt"
+sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
+
+echo "Cleaning snap"
+sudo snap remove --purge docker
+
+
+echo "Cleaning all docker files and image"
+sudo rm -rf /var/lib/docker /etc/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
+sudo rm -rf /var/lib/containerd
+sudo rm -r ~/.docker
